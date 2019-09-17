50e9b7cd22ef34904eb3480f1b22b918d6139852
Jcro15 <dkcam80@gmail.com>
2019-09-17 10:45:01 -0500

    partes 2 y 3

--- servlet/src/main/java/edu/eci/cvds/servlet/EjemploServlet.java
+++ servlet/src/main/java/edu/eci/cvds/servlet/EjemploServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.net.MalformedURLException;
+import java.io.FileNotFoundException;
 import javax.servlet.http.HttpServletResponse;
 import edu.eci.cvds.servlet.model.Todo;
 import java.util.*;
@@ -22,20 +23,24 @@ public class EjemploServlet extends HttpServlet{
     @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
        Writer responseWriter = resp.getWriter();
-       Optional<String> optName = Optional.ofNullable(req.getParameter("ids"));
-       String ids = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : null;
-       int id=-1;
+       Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
+       String id = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : null;
+       int ids=-1;
+	   resp.setContentType("text/html");
 	try{
-	id=Integer.parseInt(ids);
-	Todo todo=Service.getTodo(id);
-	resp.setContentType("text/html");
+	ids=Integer.parseInt(id);
+	Todo todo=Service.getTodo(ids);
 	resp.setStatus(HttpServletResponse.SC_OK);
 	ArrayList<Todo> todos=new ArrayList<Todo>();
 	todos.add(todo);
-        responseWriter.write(Service.todosToHTMLTable(todos));
+    responseWriter.write(Service.todosToHTMLTable(todos));
 	}
 	catch(NumberFormatException e){
 	resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+	}
+	catch(FileNotFoundException e){
+		resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		 responseWriter.write(NotFoundMessage());
 	}
 	catch(MalformedURLException e){
 	resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);	
@@ -43,10 +48,50 @@ public class EjemploServlet extends HttpServlet{
 	catch(Exception e){
 	resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 	}
-	
-		
        //resp.setStatus(HttpServletResponse.SC_OK);
        //responseWriter.write("Hello" + id + "!");
        responseWriter.flush();
    }
+   
+   @Override
+   protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException{
+	   Writer responseWriter = resp.getWriter();
+       Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
+       String id = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : null;
+       int ids=-1;
+	   resp.setContentType("text/html");
+	try{
+	ids=Integer.parseInt(id);
+	Todo todo=Service.getTodo(ids);
+	resp.setStatus(HttpServletResponse.SC_OK);
+	ArrayList<Todo> todos=new ArrayList<Todo>();
+	todos.add(todo);
+    responseWriter.write(Service.todosToHTMLTable(todos));
+	}
+	catch(NumberFormatException e){
+	resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+	}
+	catch(FileNotFoundException e){
+		resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		 responseWriter.write(NotFoundMessage());
+	}
+	catch(MalformedURLException e){
+	resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);	
+	}
+	catch(Exception e){
+	resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+	}
+       //resp.setStatus(HttpServletResponse.SC_OK);
+       //responseWriter.write("Hello" + id + "!");
+       responseWriter.flush();
+	   
+   }
+	   
+	
+   private String NotFoundMessage(){
+	  String mensaje="<!DOCTYPE html> <html> <head> <title>Start Page</title></head><body><h1>No existe un item con el identificador dado</h1></body></html>";
+	  return mensaje; 
+	 
+   }
+   
 }
